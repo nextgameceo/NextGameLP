2b02f4f7b8dac08709e1113d8bf59e36296e3e4b
aya <116853251+yamamoto-ayano@users.noreply.github.com>
2025-12-26 21:57:17 +0900

    buildできるように

--- middleware.ts
+++ middleware.ts
@@ -17,5 +17,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/news/:path*', '/business','recruit'],
+  matcher: ['/news/:path*', '/business', '/members', '/recruit'],
 };
